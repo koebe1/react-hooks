5445bf0f457a6b2a3ff4f77681408b59310843aa
koebe1 <koe.be.pay@web.de>
2021-11-05 09:42:58 +0100

    useState

--- src/components/button.js
+++ src/components/button.js
@@ -1,18 +1,28 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const Button = () => {
-
-  const [count, setCount] = useState(10);
+  const [{ count1, count2 }, setCount] = useState({ count1: 0, count2: 0 });
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button
+        onClick={() =>
+          setCount(currentState => ({
+            count1: currentState.count1 + 1,
+            count2: currentState.count2 + 2
+          }))
+        }
+      >
+        +
+      </button>
       <div style={{ display: "inline", marginLeft: "1vw" }} className="App">
-        {count}
+        count 1: {count1}
+      </div>
+      <div style={{ display: "inline", marginLeft: "1vw" }} className="App">
+        count 2: {count2}
       </div>
     </div>
   );
 };
-
 
 export default Button;
