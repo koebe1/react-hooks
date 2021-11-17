834c8ae5c05288305b7f8465baf05b4aa2b310c8
koebe1 <koe.be.pay@web.de>
2021-11-17 11:23:59 +0100

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import "./App.css";
-import React, { useState, useCallback } from "react";
-import { Hello } from "./components/Hello";
+import React, { useState, useMemo, useEffect } from "react";
+
+// useMemo good for optimizing computed values
+// --> only recompute if inputs change
+// start without useMemo and try after with it to compare
+
+const computeLongestWord = arr => {
+  if (!arr) {
+    return [];
+  }
+
+  console.log("computing longest word!");
+
+  let longestWord = "";
+
+  arr.forEach(sentence =>
+    sentence.split(" ").forEach(word => {
+      if (word.length > longestWord.length) {
+        longestWord = word;
+      }
+    })
+  );
+
+  return longestWord;
+};
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
 
-  // function now only gets recreated if count or setCount changes
-  const increment = useCallback(() => {
-    setCount(prev => prev + 1);
-  }, [setCount]);
+  const url =
+    "https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json";
+
+  // fetching api data
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const result = await res.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, [url]);
+
+  // first argument the function with the expensive computation
+  // second argument are the dependencies
+  const longestWord = useMemo(() => computeLongestWord(data), [
+    computeLongestWord,
+    data
+  ]);
 
   return (
     <div style={{ marginLeft: "2vw", marginTop: "2vw" }}>
-      <Hello increment={increment} />
-
-      <div>count: {count} </div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(prev => prev + 1)}>increment</button>
+      <div>{longestWord}</div>
     </div>
   );
 };
